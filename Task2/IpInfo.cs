c01cc083383e1ae48183b16084a55829fc640e85
masiuko@ukr.net <masiuko@ukr.net>
2021-10-12 18:25:36 +0300

    Oleh Masiuk

---
+++ Task2/IpInfo.cs
@@ -0,0 +1,115 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SigmaTest
+{
+    enum Days { monday = 1, tuesday, wednesday, thursday, friday, saturday, sunday }
+    class IpInfo
+    {
+        public string IpName { get; set; }
+        //List для динамічних масивів
+        //ми не знаємо неперед, скільки буде даних
+        List<Days> days;
+        List<int> times;
+
+        public IpInfo()
+        {
+            days = new List<Days>();
+            times = new List<int>();
+        }
+
+        public void AddDay(string day)
+        {
+            Days temp;
+            if (!Enum.TryParse(day, out temp))
+            {
+                throw new ArgumentException("Wrong day");
+            }
+            days.Add(temp);
+        }
+
+        public void AddTime(int time)
+        {
+            if (time < 0 || time > 23)
+            {
+                throw new ArgumentException("Wrong hour");
+            }
+            times.Add(time);
+        }
+
+        public int GetRepeatNumber()
+        {
+            return days.Count;
+        }
+        public string GetMostPopularDay()
+        {
+            string res = "";
+            if (days.Count == 0)
+                return "There is no popular days";
+            int maxCount = -1;
+            foreach (Days dayOfWeek in Enum.GetValues(typeof(Days)))
+            {
+                int curentCount = 0;
+                for (int i = 0; i < days.Count; i++)
+                {
+                    if (days[i] == dayOfWeek)
+                    {
+                        curentCount++;
+                    }
+                }
+                if (curentCount >= maxCount)
+                {
+                    maxCount = curentCount;
+                    res = Enum.GetName(typeof(Days), dayOfWeek);
+
+                }
+            }
+            return res;
+        }
+
+        public string GetMostPopularTime()
+        {
+            string res = "There is no popular time";
+            int maxCount = -1;
+            for (int hour = 0; hour <= 24; hour++)
+            {
+                int tempCount = 0;
+                for (int i = 0; i < times.Count; i++)
+                {
+                    if (times[i] == hour)
+                    {
+                        tempCount++;
+                    }
+                }
+                if (tempCount >= maxCount)
+                {
+                    maxCount = tempCount;
+                    res = hour.ToString();
+                }
+            }
+            return res;
+        }
+
+        public override string ToString()
+        {
+            string res = "";
+            res += "IP Name: " + IpName;
+            res += "\nHours repeat: ";
+            for (int i = 0; i < times.Count; i++)
+            {
+                res += times[i] + "  ";
+            }
+            res += "\nDays repeat: ";
+            for (int i = 0; i < times.Count; i++)
+            {
+                res += Enum.GetName(typeof(Days), days[i]) + "  ";
+            }
+            res += "\n\n";
+            return res;
+        }
+    }
+}
+
---
+++ Task2/VisitingStatistic.cs
@@ -0,0 +1,153 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SigmaTest
+{
+    class VisitingStatistic
+    {
+        List<IpInfo> arrIp;
+
+        public IpInfo this[int index]
+        {
+            get
+            {
+                if ((index >= arrIp.Count) || (index < 0))
+                    throw new IndexOutOfRangeException("Wrong index");
+                return arrIp[index];
+            }
+        }
+
+        public VisitingStatistic()
+        : this("N/A") { }
+        public VisitingStatistic(string path)
+        {
+            arrIp = new List<IpInfo>();
+            ReadFromFile(path);
+        }
+
+        public void ReadFromFile(string path)
+        {
+            try
+            {
+                if (File.Exists(path))
+                {
+                    using (StreamReader reader = new StreamReader(path))
+                    {
+                        string line;
+
+                        //відповідає чи не було повторів IP
+                        int indexRepeat;
+                        //поки не дійдемо кінця файлу
+                        while ((line = reader.ReadLine()) != null)
+                        {
+                            indexRepeat = -1;
+                            string[] lineSplit = line.Split();
+                            if (lineSplit.Length != 3)
+                                throw new ArgumentException("Wrong text input");
+
+                            //отримуємо потрібні дані
+                            //ім'я
+                            string ipName = lineSplit[0];
+                            if (ipName.Length < 14)
+                            {
+                                throw new ArgumentException("Wrong name input");
+                            }
+                            //година
+                            string[] str_time = lineSplit[1].Split(':');
+                            if (str_time.Length != 3)
+                                throw new ArgumentException("Wrong time input");
+
+                            int hour;
+                            if (!Int32.TryParse(str_time[0], out hour) || (hour < 0) || (hour > 23))
+                                throw new ArgumentException("Wrong hour input");
+
+                            //день тижня
+                            string day = lineSplit[2];
+
+
+                            //перевіряємо чи вже записаний ip
+                            for (int k = 0; k < arrIp.Count; k++)
+                            {
+                                if ((arrIp[k].IpName == ipName))
+                                {
+                                    indexRepeat = k;
+                                    break;
+                                }
+                            }
+                            //якщо  нема то додати елемент
+                            if (indexRepeat == -1)
+                            {
+                                arrIp.Add(new IpInfo());
+                                int lastIndex = arrIp.Count - 1;
+                                arrIp[lastIndex].IpName = ipName;
+                                arrIp[lastIndex].AddDay(day);
+                                arrIp[lastIndex].AddTime(hour);
+                            }
+                            //якщо є то оновити дані
+                            else
+                            {
+                                arrIp[indexRepeat].AddDay(day);
+                                arrIp[indexRepeat].AddTime(hour);
+                            }
+                        }
+                    }
+                }
+                else
+                {
+                    throw new FileNotFoundException("File not found");
+                }
+            }
+            catch (FormatException ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            catch (FileNotFoundException ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            catch (ArgumentException ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+        }
+
+        public string GetMostPopularTimeOverall()
+        {
+            string res = "There is no popular time";
+            int maxCount = -1;
+            for (int hour = 0; hour <= 24; hour++)
+            {
+                int tempCount = 0;
+                for (int i = 0; i < arrIp.Count; i++)
+                {
+                    if (Int32.Parse(arrIp[i].GetMostPopularTime()) == hour)
+                    {
+                        tempCount++;
+                    }
+                }
+                if (tempCount >= maxCount)
+                {
+                    maxCount = tempCount;
+                    res = hour.ToString();
+                }
+            }
+
+
+            return res;
+        }
+
+        public override string ToString()
+        {
+            string res = "All info\n";
+            for (int i = 0; i < arrIp.Count; i++)
+            {
+                res += arrIp[i].ToString();
+            }
+            return res;
+        }
+    }
+}
